3f3ca76bcb596d5f6c89e8e746b069490f78e9aa
jeiss <jeissoncortes02@outlook.com>
2023-02-11 16:31:35 -0500

    adicionando pruebas unitarias

---
+++ src/test/java/com/jeisson/masiveupload/PersonServiceTest.java
@@ -0,0 +1,43 @@
+package com.jeisson.masiveupload;
+
+import com.jeisson.masiveupload.model.Person;
+import com.jeisson.masiveupload.service.PersonService;
+import com.jeisson.masiveupload.service.gateways.CsvReadGateway;
+import com.jeisson.masiveupload.service.gateways.JdbcRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class PersonServiceTest {
+    @Autowired
+    private PersonService personService;
+
+    @MockBean
+    private CsvReadGateway csvReadGateway;
+
+    @MockBean
+    private JdbcRepository jdbcRepository;
+
+    @Test
+    public void testSavePerson() {
+        //prepare
+        var fileMock = new MockMultipartFile("file", "test.csv", "text/plain",
+                "".getBytes());
+        var data = List.of(new Person("John ", "Leonel", "123456789"));
+        when(csvReadGateway.readCsv(fileMock)).thenReturn(data);
+        doNothing().when(jdbcRepository).savePerson(data);
+        //execute
+        personService.savePerson(fileMock);
+        //assert
+        verify(csvReadGateway).readCsv(fileMock);
+        verify(jdbcRepository).savePerson(data);
+    }
+}
